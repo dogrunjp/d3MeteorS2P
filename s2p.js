2b8ea281e4e1965e3cfd5baf405fca225087ebac
oec <oec@Poodle.local>
2015-02-14 13:45:45 +0900

    edit for meteor simple app

--- s2p.js
+++ s2p.js
@@ -1,67 +1,91 @@
-var Growthes = new Meteor.Collection('growthes');
-if (Meteor.isClient) {
- Template.chart.rendered = function(){
- var self = this;
- // d3 code here
-  var margin= {top:30, right:30, left: 80, bottom: 50},
-        width = 760 - margin.left - margin.right, height = 400 - margin.top - margin.bottom;
+Growthes = new Mongo.Collection('growthes');
 
- var condition = {"ng":"orange", "ok":"blue", "me":"red"}
-
- //setup x, parameter of tempreature
- var xValue = function(d){return d.temp;}, //value accesseor - returns the value to encode
-        xScale = d3.scale.linear().range([0,width]),//scale - maps value to a visual display encoding
-        xMap = function(d){return xScale(xValue(d))}, // map function - maps from data value to display value
-
- //setup y, parameter of light
- var yValue = function(d){return d.light;},
-        yScale = d3.scale.linear().range([height, 0]),
-        yMap = function(d){return yScale(yValue(d));},
-
- //setup fill color
- var cValue = function(d){return d.result},
-        color = function(d){return condition2[d]};
-
- //create graph canvas
- var svg = d3.select("#sp").append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
- //
- self.handle = Meteor.autorun(function(){
-  var data = Growthes.findOne().data;
-    data.forEach(function(d){
-        d.temp = +d.temp;
-        d.light = +d.light;
-    });
-    //avoid overlapping axis, reduce and add few pixcels from domain
-    xScale.domain([d3.min(data,xValue) - 2, d3.max(data, xValue) + 2]);
-    yScale.domain([d3.min(data,yValue) - 10, d3.max(data, yValue) + 10]);
-    //draw dot
-    svg.selectAll(".dot")
-                .data(data)
-                .enter().append("circle")
-                .attr("r", 3.5)
-                .attr("cx", xMap)
-                .attr("cy", yMap)
-                .style("fill", function(d){return condition[d.result];});
-
- }//Meteor.autorun & drawPlot//
-
- }//rendered//
-}//isClient//
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-    Growthes.remove({});
-    var data = [
-        {x:"20",y:"1000",d:"1",s:"0"},
-        {x:"10",y:"900",d:"1",s:"0"},
-        {x:"15",y:"500",d:"2",s:"0"},
-        {x:"8",y:"1500",d:"2",s:"0"}
-        ];
-    data.forEach(function(grow){
-        Growthes.insert(grow);
-  });
+    Meteor.startup(function () {
+        if(Growthes.find().count() === 0) {
+            var data = [
+                {x:"200",y:"100",cat:"1",flg:false},
+                {x:"100",y:"90",cat:"1",flg:true},
+                {x:"150",y:"50",cat:"2",flg:false},
+                {x:"80",y:"150",cat:"2",flg:false}
+                ];
+            for (var i = 0; i < data.length; i++) {
+                Growthes.insert(data[i]);
+            }
+        }
+        // Growthesのdocuments全てをサーバからpublish
+        //collectionを全共有しない場合は、meteor remove autopublishし明示的にpublishしておく
+        Meteor.publish("all-growth", function(){
+            return Growthes.find(); //全て
+        });
+
+        // コンソールやclientからのMeteor.call('removeAllGrowth')でGreowthes.removeされる。
+        //collectionはブラウザに残り続けるので。。気持ち悪い場合はコンソールより実行してください。
+        return Meteor.methods({
+            removeAllGrowth: function(){
+                return Growthes.remove({});
+            }
+        });
+    })
+}
+
+if (Meteor.isClient) {
+    //subscribeを宣言しcollectionをerver-clientでミラーリング
+    Meteor.subscribe("all-growth");
+    Template.chart.events({
+        'click rect':function(e){
+            var dId = e.target.id;
+            var dFlg = e.target.getAttribute('data-selected');
+            Session.set({'selectedId': dId,'dataIsSelected': dFlg});
+            thisFlag = Growthes.findOne({_id: dId},{fields:{flg:1}}); //取得したIDから一致するdocumentのflg値を取得（_id+flgのobjectが得られる）
+            Growthes.update({_id:dId},{$set:{flg:!thisFlag.flg}}); //collectionのflgを現在の逆の値でアップデートする
+            //console.log(dFlg + ":" + dId);
+        }
+    });
+
+    Template.chart.rendered = function() {
+        svg = d3.select("#sp").append("svg")
+            .attr("width", 600)
+            .attr("height", 300)
+            .append("g");
+
+        //template.renderedで最初に一度＆collectionに変更が有ったときに呼び出される
+        Tracker.autorun(function(){
+            data = Growthes.find().fetch();
+            data.forEach(function (d) {
+                d.x = +d.x;
+                d.y = +d.y;
+            });
+            console.log(data)
+
+            svg.selectAll("g")
+                .data(data)
+                .enter()
+                .append("rect");
+
+            data.forEach(function (d) {
+                d.x = +d.x;
+                d.y = +d.y;
+             });
+            console.log(data);
+            svg.selectAll("rect")
+                .data(data)
+                .attr("x", function(d, i){return i * 38})
+                .attr("width", 35)
+                .attr("y", function(d){return +d.y})
+                .attr("height", function(d){return 300 - d.y})
+                .attr("fill", function(d){
+                    if(d.flg == true){
+                        return "orange"
+                    }else{
+                        return "steelBlue"
+                    }
+                })
+                .attr("id", function(d){return d._id})
+                .attr("data-selected", function(d){return d.flg})
+                .attr("transform", "translate(30,30)");
+
+        })
+
+    }
 }
